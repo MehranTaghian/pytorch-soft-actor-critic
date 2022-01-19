b6276cb7e1d53957989feb83f6d2642970e14de5
mehran <taghianmehran@gmail.com>
2022-01-19 09:33:47 -0700

    scripts added

---
+++ scripts.sh
@@ -0,0 +1,7 @@
+#MAIN_FILE=/home/mehran/Documents/pytorch-soft-actor-critic/main.py
+MAIN_FILE=/local/melco3/taghianj/pytorch-soft-actor-critic/main.py
+
+
+tmux new-session -d -s v0-0 "CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name Ant-v2 --automatic_entropy_tuning True --num_steps 10000000 --start_steps 10000 --cuda --seed 0"
+
+#python $MAIN_FILE --env-name Ant-v2 --automatic_entropy_tuning True --num_steps 10000000 --cuda --seed 0
